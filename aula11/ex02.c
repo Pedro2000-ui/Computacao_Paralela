7d4fe790b60de30f1bc95857678733d1db3561d3
ph_brito <pedro.henrique@teleeventos.com.br>
2025-06-08 11:10:08 -0300

    fix: bug ao setar qtd de processos

--- aula11/ex02.c
+++ aula11/ex02.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int rank, size;
     int local_sum = 0, total_sum = 0;
-    int vetor_global[8] = {1, 2, 3, 4, 5, 6, 7, 8};  // Exemplo
+    int vetor_global[8] = {1, 2, 3, 4, 5, 6, 7, 8}; // Exemplo
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (8 % size != 0)
+    {
+        if (rank == 0)
+        {
+            printf("Erro: número de processos deve dividir 8 elementos.\n");
+        }
+        MPI_Finalize();
+        return 1;
+    }
 
     int elementos_por_processo = 8 / size;
     int *subvetor = (int *)malloc(elementos_por_processo * sizeof(int));
@@ -20,18 +31,22 @@ int main(int argc, char *argv[]) {
                 0, MPI_COMM_WORLD);
 
     // Cada processo calcula sua soma local
-    for (int i = 0; i < elementos_por_processo; i++) {
+    for (int i = 0; i < elementos_por_processo; i++)
+    {
         local_sum += subvetor[i];
     }
 
     // Topologia anel: soma circula de processo em processo
     int recv_sum;
-    if (rank == 0) {
+    if (rank == 0)
+    {
         total_sum = local_sum;
         MPI_Send(&total_sum, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         MPI_Recv(&total_sum, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Soma total = %d\n", total_sum);
-    } else {
+    }
+    else
+    {
         MPI_Recv(&recv_sum, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         recv_sum += local_sum;
         int destino = (rank + 1) % size;
